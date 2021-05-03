2aea0320f267f7c0951502c532d42552d2c9c32e
normabunton <te_nj25@hotmail.com>
2021-05-03 10:28:28 -0400

    new assembly of Data

--- GarryBoats/Models/User.cs
+++ GarryBoats/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,20 @@ namespace GarryBoats.Models
 {
     public class User
     {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string FullName { get; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string  EmailAddress { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
     }
 }

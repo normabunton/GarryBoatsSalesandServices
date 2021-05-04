c4f7b8501bc780b5da0965fb51d431ec63f9a9a5
normabunton <te_nj25@hotmail.com>
2021-05-04 09:03:12 -0400

    repairclass set

---
+++ GarryBoats.Data/Repair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarryBoats.Data
+{
+    public class Repair
+    {
+        [Key]
+        public int RepairId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        [Display(Name ="Your Repair Details")]
+        public string RepairDetails { get; set; }
+        [Required]
+        public string location { get; set; }
+        [Required]
+        public string Product { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
